b7d2ace6d7b82e9a62733f271b53b6fb59296603
Tulsina0509 <74660837+Tulsina0509@users.noreply.github.com>
2021-04-22 17:26:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -28,12 +28,17 @@ function gotPoses(results)
         console.log(results);
        noseX =  results[0].pose.nose.x;
        noseY =  results[0].pose.nose.y;
+       console.log("nose x =" + noseX);
+       console.log("nose y =" + noseY);
     }
 }
 
 
 function draw() {
 image(video,0,0,300,300);
+//fill(255,0,0);
+//stroke(255,0,0);
+//circle(noseX, noseY, 20 );
 image(clown_nose, noseX, noseY, 30, 30);
 }
 
